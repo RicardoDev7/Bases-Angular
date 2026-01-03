15a91c527fdaf069cd6d0f126e66d861e0a9bb1f
Ricardo Martin Metzger Mejia <ricardo7.dev@gmail.com>
2026-01-02 23:41:28 -0500

    feat: uso de signals en angular

--- src/app/pages/counter/counter.ts
+++ src/app/pages/counter/counter.ts
@@ -1,19 +1,23 @@
-import { Component } from "@angular/core";
+import { Component, signal, WritableSignal } from "@angular/core";
 
 @Component({
   templateUrl: '../counter/counter.html',
   styleUrls: ['../counter/counter.css']
 })
 export class Counter {
-  counter = 1;
+  counter : number = 1;
+  counterSignal: WritableSignal<number> = signal(1);
   increaseByOne() : void {
     this.counter += 1;
+    this.counterSignal.update(current => current + 1);
   }
   decreaseByOne() : void {
     if(this.counter == 0) return;
     this.counter -= 1;
+    this.counterSignal.update(current => current - 1);
   }
   resetCounter() : void {
     this.counter = 1;
+    this.counterSignal.set(1);
   }
 }
