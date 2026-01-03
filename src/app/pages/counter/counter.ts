cc49e6d196e4d1802430defa53063e093c9cdda1
Ricardo Martin Metzger Mejia <ricardo7.dev@gmail.com>
2026-01-02 23:34:01 -0500

    feat: url y estilos independientes

--- src/app/pages/counter/counter.ts
+++ src/app/pages/counter/counter.ts
@@ -1,15 +1,19 @@
 import { Component } from "@angular/core";
 
 @Component({
-  template: `
-  <h1>Hello World</h1>
-  <h2>Counter APP: {{ counter }}</h2>
-  <button (click)="increaseByOne(1)">1</button>
-  `
+  templateUrl: '../counter/counter.html',
+  styleUrls: ['../counter/counter.css']
 })
 export class Counter {
-  counter = 10;
-  increaseByOne(value: number) : void {
-    this.counter += value;
+  counter = 1;
+  increaseByOne() : void {
+    this.counter += 1;
+  }
+  decreaseByOne() : void {
+    if(this.counter == 0) return;
+    this.counter -= 1;
+  }
+  resetCounter() : void {
+    this.counter = 1;
   }
 }
