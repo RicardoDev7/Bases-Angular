476b18788edf8a21c67c1e90aedee2ccca93508e
Ricardo Martin Metzger Mejia <ricardo7.dev@gmail.com>
2026-01-04 20:41:19 -0500

    feat: navbar parte 2

--- src/app/components/shared/navbar/navbar.ts
+++ src/app/components/shared/navbar/navbar.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
   styleUrl: './navbar.css',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.html'
 })
 export class Navbar {
