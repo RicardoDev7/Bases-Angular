37db554b0a40089e470b863effddf90b8e150f94
Ricardo Martin Metzger Mejia <ricardo7.dev@gmail.com>
2026-01-06 00:11:21 -0500

    feat: @for en angular

--- src/app/pages/dragon-ball/dragon-ball.ts
+++ src/app/pages/dragon-ball/dragon-ball.ts
@@ -1,9 +1,19 @@
 
-import { Component } from "@angular/core";
+import { Component, signal, WritableSignal } from '@angular/core';
+
+interface Character {
+  id: number;
+  name: string;
+  power: number;
+}
 
 @Component({
   templateUrl: '../dragon-ball/dragon-ball.html'
 })
 export class DragonBall {
-
+  characters: WritableSignal<Character[]> = signal<Character[]>([
+    { id: 1, name: 'Goku', power: 9001 },
+    { id: 2, name: 'Vegeta', power: 8500 },
+    { id: 3, name: 'Piccolo', power: 7000 }
+  ]);
 }
