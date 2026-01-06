44a8b518d87ebc28cc2dce6f90c83597a8322bfa
Ricardo Martin Metzger Mejia <ricardo7.dev@gmail.com>
2026-01-06 00:27:41 -0500

    feat: @if en angular

--- src/app/pages/dragon-ball/dragon-ball.ts
+++ src/app/pages/dragon-ball/dragon-ball.ts
@@ -16,7 +16,8 @@ export class DragonBall {
   characters: WritableSignal<Character[]> = signal<Character[]>([
     { id: 1, name: 'Goku', power: 9001 },
     { id: 2, name: 'Vegeta', power: 8500 },
-    { id: 3, name: 'Piccolo', power: 7000 }
+    { id: 3, name: 'Piccolo', power: 7000 },
+    { id: 4, name: 'Yancha', power: 500 }
   ]);
   powerClasses = computed(() => {
     return (power: number) => ({
