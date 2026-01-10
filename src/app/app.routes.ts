5318ca888caae7b223ece596767e1074669ab871
Ricardo Martin Metzger Mejia <ricardo7.dev@gmail.com>
2026-01-09 23:21:56 -0500

    feat: componente dragon ball super

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { Counter } from './pages/counter/counter';
 import { Hero } from './pages/hero/hero';
 import { DragonBall } from './pages/dragon-ball/dragon-ball';
+import { DragonBallSuper } from './pages/dragon-ball-super/dragon-ball-super';
 
 export const routes: Routes = [
   {
@@ -15,6 +16,10 @@ export const routes: Routes = [
   {
     path: 'dragonball',
     component: DragonBall
+  },
+  {
+    path: 'dragonball-super',
+    component: DragonBallSuper
   },
   {
     path: '**',
---
+++ src/app/pages/dragon-ball-super/dragon-ball-super.ts
@@ -0,0 +1,55 @@
+
+import { NgClass } from '@angular/common';
+import { Component, computed, signal, WritableSignal } from '@angular/core';
+
+interface Character {
+  id: number;
+  name: string;
+  power: number;
+}
+
+@Component({
+  templateUrl: '../dragon-ball-super/dragon-ball-super.html',
+  imports: [NgClass],
+  template: ``
+})
+export class DragonBallSuper {
+
+  name: WritableSignal<string> = signal('');
+  power: WritableSignal<number> = signal(0);
+
+  characters: WritableSignal<Character[]> = signal<Character[]>([
+    { id: 1, name: 'Goku', power: 9001 }
+  ]);
+
+  powerClasses = computed(() => {
+    return (power: number) => ({
+      'text-danger': power > 9000,
+      'text-info': power <= 9000
+    })
+  });
+
+  changeName(value: string): void {
+    this.name.set(value);
+  }
+
+  changePower(value: string) : void {
+    this.power.set(Number(value));
+  }
+
+  addCharacter() : void {
+    if(!this.name() || !this.power() || this.power() <= 0) return;
+    const newCharacter: Character = {
+      id: this.characters().length + 1,
+      name: this.name(),
+      power: this.power()
+    };
+    this.characters.update(chars => [...chars, newCharacter]);
+    this.resetFields();
+  }
+
+  resetFields(){
+    this.name.set('');
+    this.power.set(0);
+  }
+}
