f12cdce441e2491864698a292c95a59c46c76a64
Ricardo Martin Metzger Mejia <ricardo7.dev@gmail.com>
2026-01-10 23:57:51 -0500

    feat: agregar 404 page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,25 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { Navbar } from "./components/shared/navbar/navbar";
+import { Component, signal, OnInit } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { Navbar } from './components/shared/navbar/navbar';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet, Navbar],
-  templateUrl: './app.html',
-  standalone: true
+  templateUrl: './app.html'
 })
+export class App implements OnInit {
 
-export class App {
   protected readonly title = signal('Ricardo Metzger');
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    const params = new URLSearchParams(window.location.search);
+    const redirect = params.get('redirect');
+
+    if (redirect) {
+      this.router.navigateByUrl(redirect);
+    }
+  }
 }
