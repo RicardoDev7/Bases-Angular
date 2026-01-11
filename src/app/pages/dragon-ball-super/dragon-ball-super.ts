e60002e9dd3df0d3c8e6a9849d2391baf8ba1530
Ricardo Martin Metzger Mejia <ricardo7.dev@gmail.com>
2026-01-10 23:02:32 -0500

    feat: efectos y local storage

--- src/app/pages/dragon-ball-super/dragon-ball-super.ts
+++ src/app/pages/dragon-ball-super/dragon-ball-super.ts
@@ -1,15 +1,8 @@
 
-import { NgClass } from '@angular/common';
-import { Component, computed, inject, signal, WritableSignal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CharacterList } from "../../components/dragonball/character-list/character-list";
 import { CharacterAdd } from "../../components/dragonball/character-add/character-add";
 import { DragonBallService } from '../../services/dragonball.service';
-
-interface Character {
-  id: number;
-  name: string;
-  power: number;
-}
 
 @Component({
   templateUrl: '../dragon-ball-super/dragon-ball-super.html',
--- src/app/services/dragonball.service.ts
+++ src/app/services/dragonball.service.ts
@@ -1,11 +1,15 @@
-import { computed, Injectable, signal, WritableSignal } from '@angular/core';
+import { computed, effect, Injectable, signal, WritableSignal } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
 
 @Injectable({ providedIn: 'root' })
 export class DragonBallService {
-    characters: WritableSignal<Character[]> = signal<Character[]>([
+  characters: WritableSignal<Character[]> = signal<Character[]>([
     { id: 1, name: 'Goku', power: 9001 }
   ]);
+
+  saveToLocalStorage = effect(() => {
+    localStorage.setItem('dragonball-characters', JSON.stringify(this.characters()));
+  });
 
   addCharacter(newCharacter: Character) : void {
     this.characters.update(chars => [...chars, newCharacter]);
